0f2f40ce961281df3caf10f9abda4f12ca48bd7c
ananya411-robinson <ananyarobinson411@gmail.com>
2026-03-18 18:37:37 +0530

    vercel requirements !!

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # Install dependencies
+echo "BUILD START"
 python3.11 -m pip install -r requirements.txt
 # Run collectstatic
 python3.11 manage.py collectstatic --noinput --clear
+echo "BUILD END"
